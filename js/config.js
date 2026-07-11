526e430ae1e5924557c01035bf6dcea59dd5c586
MuhammadHasnain1927 <MuhammadHasnain1927@gmail.com>
2026-07-12 02:41:14 +0500

    Push

---
+++ js/config.js
@@ -0,0 +1,6 @@
+const SUPABASE_URL = "https://xpjpztanzstoglnobvvi.supabase.co";
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhwanB6dGFuenN0b2dsbm9idnZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM3NjkwNjIsImV4cCI6MjA5OTM0NTA2Mn0.PkGjZlsiwTNPJ_P2ldYivANJ9ZPDEtR6KRStnqWFqC4";
+
+const APP_NAME = "MaintainIQ";
+const ORG_NAME = "SMIT Facilities";
